967d88b2bbabaae72fe9f93db23e76fe7b5bc255
chenghongtao <18729553226@163.com>
2019-07-10 17:17:43 +0800

    project init

---
+++ springCloudMain/EurekaServer/src/main/java/org/EurekaServer/EurekaServerAppLication.java
@@ -0,0 +1,13 @@
+package org.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerAppLication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerAppLication.class, args);
+	}
+}
